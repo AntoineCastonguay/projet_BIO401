ec0d35eb6b3109fd4527cbe4403dceab1f3d051e
AntoineCastonguay <castonguay.antoine.e@gmail.com>
2024-03-27 14:56:52 -0400

    changement 27/03

--- model.R
+++ model.R
@@ -5,36 +5,35 @@ library(deSolve)
 interaction_coral <- function(t, vars, parms){
   with(as.list(c(parms, vars)), {
     # Modèle interaction coral
-    dT <- - M - C
-    dM <- a*M*C - ((P/beta)*M/(M+T)) + M*T # dM/dt
-    dC <- r*T*C - dC - a*M*C  # dC/dt
-    dP <- s*P*(1 - (P/(beta*(((ca*R+cb)/kmax)*((gamma(M+T)/(1+v(M+T)))))))) # dP/dt
-    dR <- hG*C*(3 - R) - hE(1 - C)(R - 1)
+    dY <- Y - M - C # dY/dt
+    dM <- a*M*C - ((P/beta)*M/(M+Y)) + M*Y # dM/dt
+    dC <- r*Y*C - d*C - a*M*C  # dC/dt
+    dP <- s*P*(1 - (P/(beta*(((ca*R+cb)/kmax)*((delta*(M+Y)/(1+v*(M+Y)))))))) # dP/dt
+    dR <- hG*C*(3 - R) - hE*(1 - C)*(R - 1)
     
     # Résultat
-    res <- c(dM=dM, dC=dC,dP=dP,dR=dR)
+    res <- c(dY=dY, dM=dM, dC=dC, dP=dP, dR=dR)
     return(list(res))
   })
 }
 
-dessinSol <- function(ic=c(R=25,C=20), times=1,func=interaction_coral, 
+dessinSol <- function(ic=c(Y=1000,M=500,C=500,P=1000,R=1000), times=seq(1:1000),func=interaction_coral, 
                       parms=c(a=0.1,
+                              beta=50,
+                              d=0.44,
                               r=1,
                               s=0.49,
-                              ca=0.75,
-                              cb=1,
-                              kmax=1,
-                              gamma=1,
-                              v=1,
-                              hG=1,
-                              hE=1)) {
+                              ca=-(3.21),
+                              cb=3.65,
+                              kmax=10000,
+                              delta=4,557,
+                              v=0.9877,
+                              hG=0.03,
+                              hE=0.01)) {
   soln <- ode(ic, times, func, parms)
   
-  plot(x = times, y = soln[,"R"], type = "l", col = "blue", 
-       xlab = "Temps", ylab = "Données")
-  
-  # Ajouter la deuxième ligne
-  lines(x = times, y = soln[,"C"], col = "red")
-  
-  
+  #plot(x = times, y = soln[,"M"], type = "l", col = "blue",
+  # xlab = "Temps", ylab = "Données")
 }
+
+dessinSol()
--- model_simple.R
+++ model_simple.R
@@ -5,36 +5,36 @@ library(deSolve)
 interaction_coral <- function(t, vars, parms){
   with(as.list(c(parms, vars)), {
     # Modèle interaction coral
-    dT <- - M - C
-    dM <- a*M*C - ((P/beta)*M/(M+T)) + M*T # dM/dt
-    dC <- r*T*C - dC - a*M*C  # dC/dt
-    dP <- s*P*(1 - (P/(beta*(((ca*R+cb)/kmax)*((gamma(M+T)/(1+v(M+T)))))))) # dP/dt
-    dR <- hG*C*(3 - R) - hE(1 - C)(R - 1)
+    dY <- Y - M - C # dY/dt
+    dM <- a*M*C - ((P/beta)*M/(M+Y)) + gamma*M*Y # dM/dt
+    dC <- r*Y*C - d*C - a*M*C  # dC/dt
+    dP <- s*P*(1 - (P/(beta*kmax))) - f*P # dP/dt
     
     # Résultat
-    res <- c(dM=dM, dC=dC,dP=dP,dR=dR)
+    res <- c(dY=dY, dM=dM, dC=dC, dP=dP)
     return(list(res))
   })
 }
 
-dessinSol <- function(ic=c(R=25,C=20), times=1,func=interaction_coral, 
+dessinSol <- function(ic=c(Y=100,M=50,C=50,P=100), times=seq(1:1000),func=interaction_coral, 
                       parms=c(a=0.1,
+                              beta=1000,
+                              d=0.44,
                               r=1,
+                              f=0.8,
                               s=0.49,
-                              ca=0.75,
-                              cb=1,
-                              kmax=1,
-                              gamma=1,
-                              v=1,
-                              hG=1,
-                              hE=1)) {
+                              ca=-(3.21),
+                              cb=3.65,
+                              kmax=0.5,
+                              gamma = 0.8,
+                              delta=4,557,
+                              v=0.9877,
+                              hG=0.03,
+                              hE=0.01)) {
   soln <- ode(ic, times, func, parms)
   
-  plot(x = times, y = soln[,"R"], type = "l", col = "blue", 
-       xlab = "Temps", ylab = "Données")
-  
-  # Ajouter la deuxième ligne
-  lines(x = times, y = soln[,"C"], col = "red")
-  
-  
+  #plot(x = times, y = soln[,"M"], type = "l", col = "blue",
+       # xlab = "Temps", ylab = "Données")
 }
+
+dessinSol()
